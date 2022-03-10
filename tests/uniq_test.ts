e0ff05b3035c5bad391c605edd30cc0dc7d4d82d
isanasan <yfcgpsebp@gmail.com>
2022-03-10 16:34:45 +0900

    update: test case

--- tests/uniq_test.ts
+++ tests/uniq_test.ts
@@ -1,13 +1,15 @@
 import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { uniq } from "../src/uniq.ts";
-Deno.test("uniq test", () => {
-  assertEquals(
-    ["hoge", "huga"],
-    uniq(["hoge", "huga", "hoge", "huga", "hoge"]),
-  );
-
+Deno.test("simple", () => {
   assertEquals(
     ["hoge", "huga"],
     uniq(["hoge", "huga", "hoge"]),
+  );
+});
+
+Deno.test("multi", () => {
+  assertEquals(
+    ["hoge", "huga"],
+    uniq(["hoge", "huga", "hoge", "huga", "hoge"]),
   );
 });
