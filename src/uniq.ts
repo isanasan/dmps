a0249c390768f7c52006969980786c72aa3cf801
isanasan <yfcgpsebp@gmail.com>
2022-03-10 11:56:46 +0900

    update: uniq

--- src/uniq.ts
+++ src/uniq.ts
@@ -1,3 +1,13 @@
 export function uniq(hoge: string[]): string[] {
-  return hoge;
+  const size = hoge.length;
+  const result: string[] = [];
+  for (let i = 0; i < size - 1; i++) {
+    for (let j = i + 1; j < size; j++) {
+      if (hoge[i] === hoge[j]) {
+        break;
+      }
+    }
+    result.push(hoge[i]);
+  }
+  return result;
 }
--- tests/uniq_test.ts
+++ tests/uniq_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { uniq } from "../src/uniq.ts";
 Deno.test("uniq test", () => {
-  assertEquals(["hoge", "huga"], uniq(["hoge", "huga"]));
+  assertEquals(["hoge", "huga"], uniq(["hoge", "huga", "hoge"]));
 });
