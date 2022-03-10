b34c4d220b41b1982a61e349bda84860323f18d3
isanasan <yfcgpsebp@gmail.com>
2022-03-10 10:54:56 +0900

    update: test name

--- tests/uniq_test.ts
+++ tests/uniq_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { uniq } from "../src/uniq.ts";
-Deno.test("can make stat by simple-log", () => {
+Deno.test("uniq test", () => {
   assertEquals("hoge", uniq("hoge"));
 });
