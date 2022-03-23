14d8ac56e7a1c25c42b4b3b0af25a3c42553baae
isanasan <yfcgpsebp@gmail.com>
2022-03-23 09:52:36 +0900

    refact: create stat 分離

--- src/stat-command.ts
+++ src/stat-command.ts
@@ -1,6 +1,6 @@
 import { PullRequest } from "./entity.ts";
-import { median as _median } from "https://esm.sh/mathjs";
 import { fetchAllMergedPullRequests } from "./github.ts";
+import { createStat } from "./create-stat.ts";
 
 interface StatCommandOptions {
   input: string | undefined;
@@ -43,72 +43,4 @@ export function createPullRequestsByLog(path: string): PullRequest[] {
         p.firstReviewedAt,
       ),
   );
-}
-
-interface PullRequestStat {
-  count: number;
-  authorCount: number;
-  additionsAverage: number;
-  additionsMedian: number;
-  deletionsAverage: number;
-  deletionsMedian: number;
-  leadTimeSecondsAverage: number;
-  leadTimeSecondsMedian: number;
-  timeToMergeSecondsAverage: number;
-  timeToMergeSecondsMedian: number;
-  timeToMergeFromFirstReviewSecondsAverage: number;
-  timeToMergeFromFirstReviewSecondsMedian: number;
-}
-
-export function createStat(prs: PullRequest[]): PullRequestStat {
-  const leadTimes = prs.map((pr) => pr.leadTimeSeconds);
-  const timeToMerges = prs.map((pr) => pr.timeToMergeSeconds);
-  const timeToMergeFromFirstReviews = prs
-    .map((pr) => pr.timeToMergeFromFirstReviewSeconds)
-    .filter((x): x is number => x !== undefined);
-
-  return {
-    count: prs.length,
-    authorCount: uniq(prs.map((pr) => pr.author)).length,
-    additionsAverage: average(prs.map((pr) => pr.additions)),
-    additionsMedian: median(prs.map((pr) => pr.additions)),
-    deletionsAverage: average(prs.map((pr) => pr.deletions)),
-    deletionsMedian: median(prs.map((pr) => pr.deletions)),
-    leadTimeSecondsAverage: Math.floor(average(leadTimes)),
-    leadTimeSecondsMedian: Math.floor(median(leadTimes)),
-    timeToMergeSecondsAverage: Math.floor(average(timeToMerges)),
-    timeToMergeSecondsMedian: Math.floor(median(timeToMerges)),
-    timeToMergeFromFirstReviewSecondsAverage: Math.floor(
-      average(timeToMergeFromFirstReviews),
-    ),
-    timeToMergeFromFirstReviewSecondsMedian: Math.floor(
-      median(timeToMergeFromFirstReviews),
-    ),
-  };
-}
-
-function average(numbers: number[]): number {
-  if (numbers.length === 0) return 0;
-  return numbers.reduce((prev, current) => prev + current) / numbers.length;
-}
-
-function median(numbers: number[]): number {
-  if (numbers.length === 0) return 0;
-  return _median(numbers);
-}
-
-function uniq(targetArray: Array<string | null>): Array<unknown> {
-  const filted = targetArray.filter((v) => v);
-
-  let size = filted.length;
-  for (let i = 0; i < size - 1; i++) {
-    for (let j = i + 1; j < size; j++) {
-      if (filted[i] === filted[j]) {
-        filted.splice(j, 1);
-        size--;
-        j--;
-      }
-    }
-  }
-  return filted;
 }
