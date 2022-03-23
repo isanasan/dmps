1e43801a2e5c2a1ccd489d8d067b66fe4bbcbb67
isanasan <yfcgpsebp@gmail.com>
2022-03-23 11:36:49 +0900

    add: new subcommand interval

--- mod.ts
+++ mod.ts
@@ -21,6 +21,15 @@ async function main(): Promise<void> {
     .option("--query <string>", "query for github search", { required: true })
     .action(statCommand);
 
+  const interval = new Command()
+    .option("--start <date>", "start date", { required: true })
+    .option("--end <date>", "end date", { required: true })
+    .option("--interval-days <date>", "interval date", { required: true })
+    .option("--query <string>", "query for github search", { required: true })
+    .action((intervalCommand) => {
+      console.log("interval");
+    });
+
   await new Command()
     .version("0.1.0")
     .option("--input <filepath>", "the input file path")
@@ -30,6 +39,7 @@ async function main(): Promise<void> {
     .action(statCommand)
     .command("log", log)
     .command("stat", stat)
+    .command("interval", interval)
     .parse(Deno.args);
 }
 
